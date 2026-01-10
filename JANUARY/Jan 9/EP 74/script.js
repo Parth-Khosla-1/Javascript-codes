25d3f198a7266d8a7152acfcf8209ff439c39d0e
Parth Khosla <khoslaparth55@gmail.com>
2026-01-10 20:25:12 +0530

    understanding the callback hell

--- JANUARY/Jan 9/EP 74/script.js
+++ JANUARY/Jan 9/EP 74/script.js
@@ -18,19 +18,23 @@ function makeHttpRequest(method, url) {
 }
 
 
-makeHttpRequest('GET', 'https://dummyjson.com/users')
-.then((usersData) => makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
-.then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
-.then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
-.then((userData) => console.log(userData))
-.catch((err) => console.log(err)) 
+// makeHttpRequest('GET', 'https://dummyjson.com/users')
+// .then((usersData) => makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
+// .then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+// .then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+// .then((userData) => console.log(userData))
+// .catch((err) => console.log(err)) 
 
-// makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
-//   makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
-//     makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
-//       makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
-//         console.log(userData);
-//       });
-//     });
-//   });
-// })
+
+makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
+  console.log(userData);
+  makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
+    console.log(postsData);
+    makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
+        console.log(commentsData);
+          makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
+        console.log(userData);
+      });
+    });
+  });
+})
