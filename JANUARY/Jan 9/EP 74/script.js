9ceaea48358c550d9239f6a0338f5636cb7cb05a
Parth Khosla <khoslaparth55@gmail.com>
2026-01-10 20:22:52 +0530

    structured the code

--- JANUARY/Jan 9/EP 74/script.js
+++ JANUARY/Jan 9/EP 74/script.js
@@ -17,6 +17,14 @@ function makeHttpRequest(method, url) {
   return promise
 }
 
+
+makeHttpRequest('GET', 'https://dummyjson.com/users')
+.then((usersData) => makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
+.then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+.then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+.then((userData) => console.log(userData))
+.catch((err) => console.log(err)) 
+
 // makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
 //   makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
 //     makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
@@ -26,10 +34,3 @@ function makeHttpRequest(method, url) {
 //     });
 //   });
 // })
-
-makeHttpRequest('GET', 'https://dummyjson.com/users')
-  .then((usersData) => makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
-  .then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
-  .then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
-  .then((userData) => console.log(userData))
-  .catch((err) => console.log(err))
