e3160105ebe7491c1e043d1b40d9a6e66ff8a9e4
Parth Khosla <khoslaparth55@gmail.com>
2026-01-10 20:27:45 +0530

    cleaned up unecessary clg commands

--- JANUARY/Jan 9/EP 74/script.js
+++ JANUARY/Jan 9/EP 74/script.js
@@ -27,11 +27,8 @@ function makeHttpRequest(method, url) {
 
 
 makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
-  console.log(usersData);
   makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
-    console.log(postsData);
-    makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
-        console.log(commentsData);
+      makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
           makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
         console.log(userData);
       });
