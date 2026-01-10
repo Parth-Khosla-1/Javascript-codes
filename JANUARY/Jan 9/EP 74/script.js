554646a1d98251696fec4c24a2960fc68e3e290d
Parth Khosla <khoslaparth55@gmail.com>
2026-01-10 20:36:58 +0530

    more clg for more clarity

--- JANUARY/Jan 9/EP 74/script.js
+++ JANUARY/Jan 9/EP 74/script.js
@@ -1,9 +1,9 @@
-function makeHttpRequest(method, url,callback) {
+function makeHttpRequest(method, url, callback) {
   const xhr = new XMLHttpRequest()
   xhr.responseType = 'json'
-xhr.addEventListener('load',(data)=>{
-  callback(xhr.response)
-})
+  xhr.addEventListener('load', (data) => {
+    callback(xhr.response)
+  })
   xhr.open(method, url)
   xhr.send()
 }
@@ -18,9 +18,12 @@ xhr.addEventListener('load',(data)=>{
 
 
 makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
+  console.log(usersData);
   makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
-      makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
-          makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
+    console.log(postsData);
+    makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
+      console.log(commentsData);
+      makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
         console.log(userData);
       });
     });
