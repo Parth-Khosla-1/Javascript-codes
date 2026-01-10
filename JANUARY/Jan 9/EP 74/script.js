4f217744420121e7d6107695bc3e41ecd941db14
Parth Khosla <khoslaparth55@gmail.com>
2026-01-10 20:33:24 +0530

    yea i fucked up

--- JANUARY/Jan 9/EP 74/script.js
+++ JANUARY/Jan 9/EP 74/script.js
@@ -1,20 +1,12 @@
-function makeHttpRequest(method, url) {
+function makeHttpRequest(method, url,callback) {
   const xhr = new XMLHttpRequest()
   xhr.responseType = 'json'
-
-  const promise = new Promise((resolve, reject) => {
-    xhr.addEventListener('load', () => {
-      resolve(xhr.response)
-    })
-    xhr.addEventListener('error', () => {
-      reject('request failed')
-    })
-  })
+  callback(xhr.response)
 
   xhr.open(method, url)
   xhr.send()
 
-  return promise
+  
 }
 
 
@@ -26,11 +18,11 @@ function makeHttpRequest(method, url) {
 // .catch((err) => console.log(err)) 
 
 
-makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
-  makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
-      makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
-          makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
-        console.log(userData);
+makeHttpRequest('GET', 'https://dummyjson.com/users', (Data) => {
+  makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (Data) => {
+      makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (Data) => {
+          makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (Data) => {
+        console.log(Data);
       });
     });
   });
