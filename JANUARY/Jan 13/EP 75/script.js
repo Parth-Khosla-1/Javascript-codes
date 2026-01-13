4444471c9e04f8dee79cfd8d7a5498c666761d97
Parth Khosla <khoslaparth55@gmail.com>
2026-01-13 23:39:19 +0530

    done ho gya khatam ez tha

--- JANUARY/Jan 13/EP 75/script.js
+++ JANUARY/Jan 13/EP 75/script.js
@@ -1,5 +1,26 @@
-fetch('https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=2000')
-.then(response => response.json())
-.then(data => {
-  console.log(data);
+// fetch('https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=2000')
+// .then(response => response.json())
+// .then(data => {
+//   console.log(data);
+// })// fetch('https://dummyjson.com/products')
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => {
+//     console.log(err);
+//   })
+
+fetch('https://dummyjsons.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    category: 'Stationary'
+    /* other product data */
+  })
 })
+.then(res => res.json())
+.then(console.log)
+.catch((err) => {
+  console.log(err);
+})
+            
