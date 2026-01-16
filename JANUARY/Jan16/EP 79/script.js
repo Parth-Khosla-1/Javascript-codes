c1f2b9437c53939f178a592be7013e4191f7debd
Parth Khosla <khoslaparth55@gmail.com>
2026-01-17 00:01:47 +0530

    we can import everythimg using *

--- JANUARY/Jan16/EP 79/script.js
+++ JANUARY/Jan16/EP 79/script.js
@@ -1,8 +1,8 @@
 import {myName} from "./usersData.js";
-import  usersData from"./usersData.js"
+import * as something from"./usersData.js"
 
-console.log(usersData);
-console.log(myName);
+console.log(something.myName);
+console.log(something.default);
 
 import a from "./productsData.js";
 
